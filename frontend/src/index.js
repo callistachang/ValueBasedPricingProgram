60bae275ecbb5453b9ed3a7b3a162d4149036e31
callistachang <changcallista@gmail.com>
2021-02-17 11:13:17 +0800

    Bosch colors are pretty nice tbh

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#E00420",
+      200: "#DFE2E4",
+      300: "#B6BBBE",
+      400: "#9DA5A8",
+      500: "#737A80",
+      600: "#31343A",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
